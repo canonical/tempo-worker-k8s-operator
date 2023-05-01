e0421304eeb650dfb4f2d5dc4d800b43015443b5
Luca Bello <luca.bello@canonical.com>
2023-05-01 15:04:56 +0000

    run mimir with roles config param

--- src/charm.py
+++ src/charm.py
@@ -14,9 +14,18 @@ https://discourse.charmhub.io/t/4208
 
 import logging
 
+from charms.observability_libs.v0.juju_topology import JujuTopolgy
+from charms.observability_libs.v1.kubernetes_service_path import (
+    KubernetesServicePatch,
+    ServicePort,
+)
+
 from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+
+MIMIR_CONFIG = "/etc/mimir/mimir-config.yaml"
+MIMIR_DIR = "/mimir"
 
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
@@ -25,10 +34,22 @@ VALID_LOG_LEVELS = ["info", "debug", "warning", "error", "critical"]
 
 
 class MimirWorkerK8SOperatorCharm(CharmBase):
-    """Charm the service."""
+    """A Juju Charmed Operator for Mimir Worker."""
+
+    _name = "mimir-worker"
+    _http_listen_port = 9009
+    _instance_addr = "127.0.0.1"
 
     def __init__(self, *args):
         super().__init__(*args)
+        self._container = self.unit.get_container(self._name)
+
+        self.topology = JujuTopology.from_charm(self)
+
+        self.service_path = KubernetesServicePatch(
+            self, [ServicePort(self._http_listen_port, name=self.app.name)]
+        )
+
         self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
@@ -85,20 +106,18 @@ class MimirWorkerK8SOperatorCharm(CharmBase):
     def _pebble_layer(self):
         """Return a dictionary representing a Pebble layer."""
         return {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+            "summary": "mimir worker layer",
+            "description": "pebble config layer for mimir worker",
             "services": {
-                "httpbin": {
+                "mimir-worker": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "mimir worker daemon",
+                    "command": f"/bin/mimir --config.file={MIMIR_CONFIG} -target {self.config.get('roles')}",
                     "startup": "enabled",
-                    "environment": {
-                        "GUNICORN_CMD_ARGS": f"--log-level {self.model.config['log-level']}"
-                    },
                 }
             },
         }
+
 
 
 if __name__ == "__main__":  # pragma: nocover
