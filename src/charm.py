06a5d0823b9c04327997475274d03e068b3df9a1
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2023-05-02 13:45:20 +0000

    basic running charm

--- src/charm.py
+++ src/charm.py
@@ -14,19 +14,17 @@ https://discourse.charmhub.io/t/4208
 
 import logging
 import re
-import socket
 from typing import Optional
 
 import yaml
 from charms.observability_libs.v0.juju_topology import JujuTopology
-from charms.observability_libs.v1.kubernetes_service_path import (
+from charms.observability_libs.v1.kubernetes_service_patch import (
     KubernetesServicePatch,
     ServicePort,
 )
-from interfaces.mimir_worker.v0.schema import ProviderSchema
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus
 from ops.pebble import PathError, ProtocolError
 
 MIMIR_CONFIG = "/etc/mimir/mimir-config.yaml"
@@ -55,65 +53,49 @@ class MimirWorkerK8SOperatorCharm(CharmBase):
             self, [ServicePort(self._http_listen_port, name=self.app.name)]
         )
 
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(self.on.mimir_worker_pebble_ready, self._on_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
-        ProviderSchema(
-            app={
-                "hash_ring": '["some_address"]',
-                "config": '{"key": "value"}',
-                "s3_config": '{"url": "dd", "endpoint": "dd", "secret_key":"SOME_KEY", "access_key":"ANOTHER_KEY", "insecure": "false"}',
-            }
+    def _on_config_changed(self, event):
+        if not self._container.can_connect():
+            return
+
+        restart = any(
+            [
+                self._update_mimir_config(),
+                self._set_pebble_layer(),
+            ]
         )
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+        if restart:
+            self.restart()
 
-        Change this example to suit your needs. You'll need to specify the right entrypoint and
-        environment configuration for your specific workload.
+    def _on_pebble_ready(self, event):
+        # Temp: Please remove when a real config file is written.
+        self._container.push(MIMIR_CONFIG, "", make_dirs=True)
 
-        Learn more about interacting with Pebble at at https://juju.is/docs/sdk/pebble.
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", self._pebble_layer, combine=True)
-        # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
-        container.replan()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+        self.unit.set_workload_version(self._mimir_version)
+        self._update_mimir_config()
+        self._set_pebble_layer()
+        self.restart()
         self.unit.status = ActiveStatus()
 
-    def _on_config_changed(self, event):
-        """Handle changed configuration.
+    def _set_pebble_layer(self) -> bool:
+        """Set Pebble layer.
 
-        Change this example to suit your needs. If you don't need to handle config, you can remove
-        this method.
-
-        Learn more about config at https://juju.is/docs/sdk/config
+        Returns: True if Pebble layer was added, otherwise False.
         """
-        # Fetch the new config value
-        log_level = self.model.config["log-level"].lower()
+        current_layer = self._container.get_plan()
+        new_layer = self._pebble_layer
 
-        # Do some validation of the configuration option
-        if log_level in VALID_LOG_LEVELS:
-            # The config is good, so update the configuration of the workload
-            container = self.unit.get_container("httpbin")
-            # Verify that we can connect to the Pebble API in the workload container
-            if container.can_connect():
-                # Push an updated layer with the new config
-                container.add_layer("httpbin", self._pebble_layer, combine=True)
-                container.replan()
+        if (
+            "services" not in current_layer.to_dict()
+            or current_layer.services != new_layer["services"]
+        ):
+            self._container.add_layer(self._name, new_layer, combine=True)
+            return True
 
-                logger.debug("Log level for gunicorn changed to '%s'", log_level)
-                self.unit.status = ActiveStatus()
-            else:
-                # We were unable to connect to the Pebble API, so we defer this event
-                event.defer()
-                self.unit.status = WaitingStatus("waiting for Pebble API")
-        else:
-            # In this case, the config option is bad, so block the charm and notify the operator.
-            self.unit.status = BlockedStatus("invalid log level: '{log_level}'")
+        return False
 
     @property
     def _pebble_layer(self):
@@ -143,31 +125,7 @@ class MimirWorkerK8SOperatorCharm(CharmBase):
             return result.group(1)
         return None
 
-    def _configure_mimir(self, event):
-        if not self._container.can_connect():
-            self.unit.status = WaitingStatus("Waiting for Pebble ready")
-            return
-
-        try:
-            self._set_alerts()
-            restart = any(
-                [
-                    self._pebble_layer_changed(),
-                    self._mimir_config_changed(),
-                ]
-            )
-
-        except BlockedStatusError as e:
-            self.unit.status = BlockedStatus(e.message)
-            return
-
-        if restart:
-            self._container.restart(self._name)
-            logger.info("Mimir (re)started")
-
-        self.unit.status = ActiveStatus()
-
-    def _mimir_config_changed(self) -> bool:
+    def _update_mimir_config(self) -> bool:
         """Set Mimir config.
 
         Returns: True if config has changed, otherwise False.
@@ -176,17 +134,13 @@ class MimirWorkerK8SOperatorCharm(CharmBase):
         """
         config = self._build_mimir_config()
 
-        try:
-            if self._running_mimir_config() != config:
-                config_as_yaml = yaml.safe_dump(config)
-                self._container.push(MIMIR_CONFIG, config_as_yaml, make_dirs=True)
-                logger.info("Pushed new Mimir configuration")
-                return True
+        if self._running_mimir_config() != config:
+            config_as_yaml = yaml.safe_dump(config)
+            self._container.push(MIMIR_CONFIG, config_as_yaml, make_dirs=True)
+            logger.info("Pushed new Mimir configuration")
+            return True
 
-            return False
-        except (ProtocolError, Exception) as e:
-            logger.error("Failed to push updated config file: %s", e)
-            raise BlockedStatusError(str(e))
+        return False
 
     def _build_mimir_config(self) -> dict:
         # TODO: Implement this!
@@ -208,18 +162,12 @@ class MimirWorkerK8SOperatorCharm(CharmBase):
             )
             return {}
 
-    @property
-    def hostname(self) -> str:
-        """Unit's hostname."""
-        return socket.getfqdn()
-
-
-class BlockedStatusError(Exception):
-    """Raised if there is an error that should set BlockedStatus."""
-
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(self.message)
+    def restart(self):
+        """Restart the pebble service or start if not already running."""
+        if self._container.get_service(self._name).is_running():
+            self._container.restart(self._name)
+        else:
+            self._container.start(self._name)
 
 
 if __name__ == "__main__":  # pragma: nocover
