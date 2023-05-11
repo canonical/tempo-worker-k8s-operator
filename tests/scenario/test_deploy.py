292c9fed00b63a48125efe47f383185396fd2be8
Pietro Pasotti <starfire.daemon@gmail.com>
2023-05-11 15:33:28 +0200

    fixed scenario tests

--- tests/scenario/test_deploy.py
+++ tests/scenario/test_deploy.py
@@ -1,71 +1,60 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-from unittest.mock import Mock, PropertyMock
-
 import pytest
-from charm import BlockedStatusError, MimirWorkerK8SOperatorCharm
-from charms.harness_extensions.v0.evt_sequences import Event, Scenario
-from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
-from ops.model import BlockedStatus, Container, WaitingStatus
-from ops.pebble import ProtocolError
+from ops.model import WaitingStatus, ActiveStatus
+from scenario import Container, State, Relation
+
+from tests.scenario.conftest import MIMIR_VERSION_EXEC_OUTPUT
 
 
-@pytest.fixture(autouse=True)
-def setup():
-    Container.can_connect = Mock(return_value=True)
-    MimirWorkerK8SOperatorCharm._mimir_version = PropertyMock(return_value="2.4.0")
-    MimirWorkerK8SOperatorCharm._current_mimir_config = PropertyMock(return_value={})
-    MimirWorkerK8SOperatorCharm._set_alerts = Mock(return_value=True)
-    KubernetesServicePatch.__init__ = Mock(return_value=None)
+@pytest.mark.parametrize('evt', ["update-status", 'config-changed'])
+def test_update_status_cannot_connect(ctx, evt):
+    mimir_container = Container('mimir-worker', can_connect=False)
+
+    state_out = ctx.run(evt, state=State(containers=[mimir_container]))
+    assert state_out.status.unit == WaitingStatus("Waiting for Pebble ready")
 
 
-def generate_scenario():
-    return Scenario.from_events(
-        ("install", "config-changed", "start", Event("mimir-worker-pebble-ready", workload=Mock()))
-    )(MimirWorkerK8SOperatorCharm)
+@pytest.mark.parametrize("relations", (
+        ["alertmanager", "compactor"],
+        ["alertmanager", "distributor"],
+        ["alertmanager"],
+        ["alertmanager", "query-frontend"],
+        ["alertmanager", "ruler", "store-gateway"],
+        ["alertmanager", "overrides-exporter", "ruler", "store-gateway"],  # order matters
+))
+def test_pebble_ready_plan(ctx, relations):
+    mimir_relations = tuple('mimir-' + relation for relation in relations)
 
-
-def test_deploy_ok_scenario():
     expected_plan = {
         "services": {
-            "mimir": {
+            "mimir-worker": {
                 "override": "replace",
                 "summary": "mimir worker daemon",
-                "command": "/bin/mimir --config.file=/etc/mimir/mimir-config.yaml -target all,alertmanager",
+                "command": f"/bin/mimir --config.file=/etc/mimir/mimir-config.yaml -target {','.join(mimir_relations)}",
                 "startup": "enabled",
             }
         },
     }
-    cc = generate_scenario().play_until_complete()
-    assert cc[2].harness.get_container_pebble_plan("mimir-worker").to_dict() == expected_plan
-    assert (
-        cc[2]
-        .harness.model.unit.get_container("mimir-worker")
-        .get_service("mimir-worker")
-        .is_running()
-        is True
-    )
-    assert cc[2].harness.charm.unit.status.name == "active"
 
-
-def test_config_changed_cannot_connect():
-    Container.can_connect = Mock(return_value=False)
-    cc = generate_scenario().play_until_complete()
-    assert cc[2].harness.model.unit.status == WaitingStatus("Waiting for Pebble ready")
-
-
-def test_deploy_and_set_alerts_error_scenario():
-    MimirWorkerK8SOperatorCharm._set_alerts = Mock(
-        side_effect=BlockedStatusError("Failed to push updated alert files; see debug logs")
-    )
-    cc = generate_scenario().play_until_complete()
-    assert cc[2].harness.charm.unit.status == BlockedStatus(
-        "Failed to push updated alert files; see debug logs"
+    mimir_container = Container(
+        'mimir-worker',
+        can_connect=True,
+        exec_mock={("/bin/mimir", "-version"): MIMIR_VERSION_EXEC_OUTPUT}
     )
 
+    state_out = ctx.run(mimir_container.pebble_ready_event,
+                        state=State(
+                            containers=[mimir_container],
+                            relations=list(Relation(endpoint,
+                                                    interface='mimir_worker',
+                                                    remote_app_name=f"{endpoint} app") for endpoint in mimir_relations)
+                        ))
 
-def test_deploy_and_cannot_push_scenario():
-    Container.push = Mock(side_effect=ProtocolError("Message"))
-    cc = generate_scenario().play_until_complete()
-    assert cc[2].harness.charm.unit.status.name == "blocked"
+    mimir_container_out = state_out.get_container(mimir_container)
+    assert mimir_container_out.services.get('mimir-worker').is_running() is True
+    assert mimir_container_out.plan.to_dict() == expected_plan
+
+    assert state_out.status.unit == ActiveStatus('')
+
