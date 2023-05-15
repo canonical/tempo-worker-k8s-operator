8ec553c6c83fea7ea1277a1a16d55eaf587f3085
sed-i <82407168+sed-i@users.noreply.github.com>
2023-05-15 17:17:10 -0400

    Lint

--- tests/scenario/test_deploy.py
+++ tests/scenario/test_deploy.py
@@ -2,30 +2,33 @@
 # See LICENSE file for licensing details.
 
 import pytest
-from ops.model import WaitingStatus, ActiveStatus
-from scenario import Container, State, Relation
+from ops.model import ActiveStatus, WaitingStatus
+from scenario import Container, Relation, State
 
 from tests.scenario.conftest import MIMIR_VERSION_EXEC_OUTPUT
 
 
-@pytest.mark.parametrize('evt', ["update-status", 'config-changed'])
+@pytest.mark.parametrize("evt", ["update-status", "config-changed"])
 def test_update_status_cannot_connect(ctx, evt):
-    mimir_container = Container('mimir-worker', can_connect=False)
+    mimir_container = Container("mimir-worker", can_connect=False)
 
     state_out = ctx.run(evt, state=State(containers=[mimir_container]))
     assert state_out.status.unit == WaitingStatus("Waiting for Pebble ready")
 
 
-@pytest.mark.parametrize("relations", (
+@pytest.mark.parametrize(
+    "relations",
+    (
         ["alertmanager", "compactor"],
         ["alertmanager", "distributor"],
         ["alertmanager"],
         ["alertmanager", "query-frontend"],
         ["alertmanager", "ruler", "store-gateway"],
         ["alertmanager", "overrides-exporter", "ruler", "store-gateway"],  # order matters
-))
+    ),
+)
 def test_pebble_ready_plan(ctx, relations):
-    mimir_relations = tuple('mimir-' + relation for relation in relations)
+    mimir_relations = tuple("mimir-" + relation for relation in relations)
 
     expected_plan = {
         "services": {
@@ -39,22 +42,24 @@ def test_pebble_ready_plan(ctx, relations):
     }
 
     mimir_container = Container(
-        'mimir-worker',
+        "mimir-worker",
         can_connect=True,
-        exec_mock={("/bin/mimir", "-version"): MIMIR_VERSION_EXEC_OUTPUT}
+        exec_mock={("/bin/mimir", "-version"): MIMIR_VERSION_EXEC_OUTPUT},
     )
 
-    state_out = ctx.run(mimir_container.pebble_ready_event,
-                        state=State(
-                            containers=[mimir_container],
-                            relations=list(Relation(endpoint,
-                                                    interface='mimir_worker',
-                                                    remote_app_name=f"{endpoint} app") for endpoint in mimir_relations)
-                        ))
+    state_out = ctx.run(
+        mimir_container.pebble_ready_event,
+        state=State(
+            containers=[mimir_container],
+            relations=[
+                Relation(endpoint, interface="mimir_worker", remote_app_name=f"{endpoint} app")
+                for endpoint in mimir_relations
+            ],
+        ),
+    )
 
     mimir_container_out = state_out.get_container(mimir_container)
-    assert mimir_container_out.services.get('mimir-worker').is_running() is True
+    assert mimir_container_out.services.get("mimir-worker").is_running() is True
     assert mimir_container_out.plan.to_dict() == expected_plan
 
-    assert state_out.status.unit == ActiveStatus('')
-
+    assert state_out.status.unit == ActiveStatus("")
