ca56dde7057f56dd396b0bd6c667101b83f7385f
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-08-28 14:55:43 +0200

    Add conftest.py for interface tests
    Add test_tempo_cluster
    
    Add KubernetesResourcePatch to patched
    
    fetch right patches as in scenario tests
    
    add role config to fixture
    
    Role config is not the issue here
    
    patch worker status
    
    proper status path
    
    patch just urlopen - seems I lobotomized the charm
    
    mock config file
    
    mock juju topology
    
    valid uuid, hopefully
    
    it wasn't
    
    use uuid4
    
    Mock methods properly
    
    Use MagicMock for urlopen in the same way as in scenario tests
    
    Add a partial urlopen patch
    
    add @contextmanager like in scenario tests
    
    patch socket.getfqdn
    
    drop socket.getfqdn() mock
    
    patch the cert-related code
    
    Remove mock on jujutopology
    
    fmt
    
    Remove commented out mocks

---
+++ tests/interface/conftest.py
@@ -0,0 +1,69 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+import tempfile
+from contextlib import contextmanager
+from functools import partial
+from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+from charms.tempo_k8s.v1.charm_tracing import charm_tracing_disabled
+from interface_tester import InterfaceTester
+from ops import ActiveStatus
+from scenario import Container, Mount, State, ExecOutput
+from unittest.mock import MagicMock
+from cosl.coordinated_workers.worker import CONFIG_FILE
+
+from charm import TempoWorkerK8SOperatorCharm
+
+
+@contextmanager
+def _urlopen_patch(url: str, resp, tls: bool = False):
+    if url == f"{'https' if tls else 'http'}://localhost:3200/ready":
+        mm = MagicMock()
+        mm.read = MagicMock(return_value=resp.encode("utf-8"))
+        yield mm
+    else:
+        raise RuntimeError("unknown path")
+
+
+# Interface tests are centrally hosted at https://github.com/canonical/charm-relation-interfaces.
+# this fixture is used by the test runner of charm-relation-interfaces to test tempo's compliance
+# with the interface specifications.
+# DO NOT MOVE OR RENAME THIS FIXTURE! If you need to, you'll need to open a PR on
+# https://github.com/canonical/charm-relation-interfaces and change tempo's test configuration
+# to include the new identifier/location.
+@pytest.fixture
+def interface_tester(interface_tester: InterfaceTester):
+    td = tempfile.TemporaryDirectory()
+    filename = "config.yaml"
+    conf_file = Path(td.name).joinpath(filename)
+    conf_file.write_text("foo: bar")
+
+    with patch.multiple(
+        "cosl.coordinated_workers.worker.KubernetesComputeResourcesPatch",
+        _namespace="test-namespace",
+        _patch=lambda _: None,
+        get_status=lambda _: ActiveStatus(""),
+    ):
+        with patch("urllib.request.urlopen", new=partial(_urlopen_patch, resp="ready")):
+            with patch("lightkube.core.client.GenericSyncClient"):
+                with patch("subprocess.run"):
+                    with charm_tracing_disabled():
+                        interface_tester.configure(
+                            charm_type=TempoWorkerK8SOperatorCharm,
+                            state_template=State(
+                                leader=True,
+                                containers=[
+                                    Container(
+                                        name="tempo",
+                                        can_connect=True,
+                                        mounts={"worker-config": Mount(CONFIG_FILE, conf_file)},
+                                        exec_mock={
+                                            ("update-ca-certificates", "--fresh"): ExecOutput(),
+                                        },
+                                    )
+                                ],
+                            ),
+                        )
+                        yield interface_tester
