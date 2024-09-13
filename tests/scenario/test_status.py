afad87d803cc4d97c4d7e56f402af9ea78bb66ae
PietroPasotti <starfire.daemon@gmail.com>
2024-09-13 11:29:31 +0200

    Remove status tests (#35)
    * removed status tests as they are now in the worker
    
    * lint

--- tests/scenario/test_status.py
+++ tests/scenario/test_status.py
@@ -3,19 +3,16 @@ from functools import partial
 from unittest.mock import patch
 
 import pytest
-from cosl.coordinated_workers.interface import ClusterProviderAppData
-from cosl.coordinated_workers.worker import CONFIG_FILE
-from ops import ActiveStatus, WaitingStatus
+from ops import ActiveStatus
 from ops import BlockedStatus
-from scenario import State, Container, Relation, Mount, Secret
+from scenario import State, Container, Relation
+from scenario.errors import UncaughtCharmError
 
 from tests.scenario.conftest import _urlopen_patch
 import json
-from unittest.mock import MagicMock
 
-from cosl.coordinated_workers.interface import ClusterRequirer
 
-from tests.scenario.conftest import TEMPO_VERSION_EXEC_OUTPUT, UPDATE_CA_CERTS_EXEC_OUTPUT
+from tests.scenario.conftest import UPDATE_CA_CERTS_EXEC_OUTPUT
 from tests.scenario.helpers import set_role
 
 tempo_container = Container("tempo", can_connect=True, execs={UPDATE_CA_CERTS_EXEC_OUTPUT})
@@ -44,183 +41,6 @@ def config_on_disk():
         yield
 
 
-def test_status_check_no_pebble(ctx, caplog):
-    # GIVEN the container cannot connect
-    db = {}
-    ClusterProviderAppData(worker_config="foo:12").dump(db)
-
-    state = State(
-        relations=[Relation("tempo-cluster", remote_app_data=db)],
-        containers=[Container("tempo")],
-    )
-    # WHEN we run any event
-    state_out = ctx.run(ctx.on.update_status(), state)
-
-    # THEN the charm sets blocked
-    assert state_out.unit_status == BlockedStatus("node down (see logs)")
-    # AND THEN the charm logs that the container isn't ready.
-    assert "Container cannot connect. Skipping status check." in caplog.messages
-
-
-def test_status_check_no_config(ctx, caplog):
-    # GIVEN there is no config file on disk
-    db = {}
-    ClusterProviderAppData(worker_config="foo:12").dump(db)
-
-    state = State(
-        relations=[Relation("tempo-cluster", remote_app_data=db)],
-        containers=[tempo_container],
-    )
-    # WHEN we run any event
-    state_out = ctx.run(ctx.on.update_status(), state)
-
-    # THEN the charm sets blocked
-    assert state_out.unit_status == BlockedStatus("node down (see logs)")
-
-
-def test_status_check_starting(ctx, tmp_path):
-    # GIVEN getting the status returns "Starting: X"
-    db = {}
-    ClusterProviderAppData(worker_config="some: yaml").dump(db)
-
-    with endpoint_starting(), config_on_disk():
-        state = State(
-            relations=[Relation("tempo-cluster", remote_app_data=db)],
-            containers=[tempo_container],
-        )
-        # WHEN we run any event
-        state_out = ctx.run(ctx.on.update_status(), state)
-    # THEN the charm sets waiting: Starting...
-    assert state_out.unit_status == WaitingStatus("Starting...")
-
-
-def test_status_check_tls(ctx, tmp_path):
-    # GIVEN getting the status returns "Starting: X" and we have TLS enabled
-    db = {}
-    ClusterProviderAppData(
-        worker_config="some: yaml",
-        # simulate tls active
-        ca_cert="cacert",
-        server_cert="servercert",
-        privkey_secret_id="privkey",
-    ).dump(db)
-
-    with endpoint_starting(tls=True), config_on_disk():
-        state = State(
-            secrets=[Secret(id="secret:privkey", tracked_content={})],
-            relations=[Relation("tempo-cluster", remote_app_data=db)],
-            containers=[tempo_container],
-        )
-        # WHEN we run any event
-        state_out = ctx.run(ctx.on.update_status(), state)
-    # THEN the charm sets waiting: Starting...
-    assert state_out.unit_status == WaitingStatus("Starting...")
-
-
-def test_status_check_ready(ctx, tmp_path):
-    # GIVEN getting the status returns "ready"
-    db = {}
-    ClusterProviderAppData(worker_config="foo:12", remote_write_endpoints=[{"url": "test"}]).dump(
-        db
-    )
-    cfg_file = tmp_path / "fake.config"
-    cfg_file.write_text("some: yaml")
-
-    with endpoint_ready(), config_on_disk():
-        state = State(
-            relations=[Relation("tempo-cluster", remote_app_data=db)],
-            containers=[
-                Container(
-                    "tempo",
-                    can_connect=True,
-                    execs={
-                        TEMPO_VERSION_EXEC_OUTPUT,
-                        UPDATE_CA_CERTS_EXEC_OUTPUT,
-                    },
-                    mounts={"cfg": Mount(location=CONFIG_FILE, source=cfg_file)},
-                )
-            ],
-        )
-        # WHEN we run any event
-        state_out = ctx.run(ctx.on.update_status(), state)
-    # THEN the charm sets waiting: Starting...
-    assert state_out.unit_status == ActiveStatus("(all roles) ready.")
-
-
-@endpoint_ready
-@patch.object(ClusterRequirer, "get_worker_config", MagicMock(return_value={"config": "config"}))
-@patch(
-    "cosl.coordinated_workers.worker.KubernetesComputeResourcesPatch.get_status",
-    MagicMock(return_value=BlockedStatus("`juju trust` this application")),
-)
-def test_patch_k8s_failed(ctx):
-
-    tempo_container = Container(
-        "tempo",
-        can_connect=True,
-        exec_mock={
-            TEMPO_VERSION_EXEC_OUTPUT,
-            UPDATE_CA_CERTS_EXEC_OUTPUT,
-        },
-    )
-    state_out = ctx.run(
-        "config_changed",
-        state=set_role(
-            State(
-                containers=[tempo_container],
-                relations=[
-                    Relation(
-                        "tempo-cluster",
-                        remote_app_data={
-                            "tempo_config": json.dumps({"alive": "beef"}),
-                        },
-                    )
-                ],
-            ),
-            "all",
-        ),
-    )
-
-    assert state_out.unit_status == BlockedStatus("`juju trust` this application")
-
-
-@endpoint_ready
-@patch.object(ClusterRequirer, "get_worker_config", MagicMock(return_value={"config": "config"}))
-@patch(
-    "cosl.coordinated_workers.worker.KubernetesComputeResourcesPatch.get_status",
-    MagicMock(return_value=WaitingStatus("")),
-)
-def test_patch_k8s_waiting(ctx):
-
-    tempo_container = Container(
-        "tempo",
-        can_connect=True,
-        exec_mock={
-            TEMPO_VERSION_EXEC_OUTPUT,
-            UPDATE_CA_CERTS_EXEC_OUTPUT,
-        },
-    )
-    state_out = ctx.run(
-        "config_changed",
-        state=set_role(
-            State(
-                containers=[tempo_container],
-                relations=[
-                    Relation(
-                        "tempo-cluster",
-                        remote_app_data={
-                            "tempo_config": json.dumps({"alive": "beef"}),
-                        },
-                    )
-                ],
-            ),
-            "all",
-        ),
-    )
-
-    assert state_out.unit_status == WaitingStatus("")
-
-
 @pytest.mark.parametrize(
     "role_str, expected",
     (
@@ -230,11 +50,6 @@ def test_patch_k8s_waiting(ctx):
                 "metrics-generator disabled. No prometheus remote-write relation configured on the coordinator"
             ),
         ),
-        ("querier", ActiveStatus("querier ready.")),
-        ("query-frontend", ActiveStatus("query-frontend ready.")),
-        ("ingester", ActiveStatus("ingester ready.")),
-        ("distributor", ActiveStatus("distributor ready.")),
-        ("compactor", ActiveStatus("compactor ready.")),
         (
             "metrics-generator",
             BlockedStatus("No prometheus remote-write relation configured on the coordinator"),
@@ -259,3 +74,47 @@ def test_status_remote_write_endpoints(role_str, expected, ctx):
     with endpoint_ready(), config_on_disk():
         state_out = ctx.run(ctx.on.collect_unit_status(), set_role(state, role_str))
         assert state_out.unit_status == expected
+
+
+@pytest.mark.parametrize(
+    "roles_enabled",
+    (
+        (
+            "all",
+            "metrics-generator",
+        ),
+        (
+            "ingester",
+            "metrics-generator",
+        ),
+        (
+            "ingester",
+            "compactor",
+            "querier",
+        ),
+    ),
+)
+def test_status_too_many_roles_enabled(roles_enabled, ctx):
+    cluster_relation = Relation(
+        "tempo-cluster",
+        remote_app_data={
+            "worker_config": json.dumps("some: yaml"),
+        },
+    )
+
+    state = State(
+        leader=True,
+        config={f"role-{role}": True for role in roles_enabled},
+        containers=[tempo_container],
+        relations=[cluster_relation],
+    )
+
+    # TODO: when https://github.com/canonical/cos-lib/pull/77 merges,
+    # we can replace this with:
+    # with endpoint_ready(), config_on_disk():
+    #     state_out = ctx.run(ctx.on.collect_unit_status(), state)
+    #     assert state_out.unit_status == BlockedStatus(f"cannot have more than 1 enabled role: {roles_enabled}")
+
+    with pytest.raises(UncaughtCharmError):
+        with endpoint_ready(), config_on_disk():
+            ctx.run(ctx.on.collect_unit_status(), state)
